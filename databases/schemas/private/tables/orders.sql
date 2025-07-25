e69b9b03643d0dd60b278615caa4cadc15a744e5
mbothe <mbothe7@hotmail.com>
2025-07-24 21:27:02 -0400

    ordser

---
+++ databases/schemas/private/tables/orders.sql
@@ -0,0 +1,9 @@
+CREATE OR ALTER TABLE PRIVATE.JSON_ORDERS_TBL_STG (
+  customer varchar,
+  order_date date,
+  delivery_date date,
+  baked_good_type varchar,
+  quantity number,
+  source_file_name varchar,
+  load_ts timestamp
+);
